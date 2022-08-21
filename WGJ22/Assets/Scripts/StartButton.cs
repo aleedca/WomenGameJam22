93342b2e9b1af2143d39c70a2822cfce7dee27d3
aleedca <acerdas1701@estudiantec.cr>
2022-08-21 17:37:25 -0600

    escena intro y modificaciones finales

--- WGJ22/Assets/Scripts/StartButton.cs
+++ WGJ22/Assets/Scripts/StartButton.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
+    //public Button button;
+    //public Sprite newSprite;
 
     public void StartGame(){
         SceneManager.LoadScene("Water");
+        //button.image.sprite = newSprite;
     }
 }
